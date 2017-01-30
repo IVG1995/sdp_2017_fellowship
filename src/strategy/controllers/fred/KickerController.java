f54c7c5852d4b8a3aa1f44d40cbe7a503fd0f993
Cole Zimmerman <czimmer112@gmail.com>
2017-01-30 18:09:33 +0000

    Added the skeleton for the kicker controller; added it to FRED's list of controllers

---
+++ src/strategy/controllers/fred/KickerController.java
@@ -0,0 +1,19 @@
+package strategy.controllers.fred;
+
+import strategy.controllers.ControllerBase;
+import strategy.robots.RobotBase;
+/**
+ * Created by cole on 1/30/17.
+ */
+public class KickerController extends ControllerBase {
+
+    public KickerController(RobotBase robot) {
+        super(robot);
+    }
+
+    public void perform() {
+
+    }
+
+
+}
--- src/strategy/robots/Fred.java
+++ src/strategy/robots/Fred.java
@@ -1,6 +1,7 @@
 package strategy.robots;
 
 import strategy.controllers.fred.PropellerController;
+import strategy.controllers.fred.KickerController;
 import strategy.drives.FourWheelHolonomicDrive;
 import communication.ports.robotPorts.FredRobotPort;
 import vision.RobotType;
@@ -12,9 +13,12 @@ public class Fred extends RobotBase {
 
     public final PropellerController PROPELLER_CONTROLLER = new PropellerController(this);
 
+    public final KickerController KICKER_CONTROLLER = new KickerController(this);
+
     public Fred(RobotType robotType){
         super(robotType, new FredRobotPort(), new FourWheelHolonomicDrive());
         this.controllers.add(this.PROPELLER_CONTROLLER);
+        this.controllers.add(this.KICKER_CONTROLLER);
     }
 
 
