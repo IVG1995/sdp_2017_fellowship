aa5abfefbd363b5254e753473974b62ffb8625fa
Ivaylo Genev <s1422179@remendado.inf.ed.ac.uk>
2017-02-24 15:55:29 +0000

    Modified FourWheelHolonomicDrive to account for difference in front-back axis (8cm) and left-right axis (10cm).

--- src/strategy/drives/FourWheelHolonomicDrive.java
+++ src/strategy/drives/FourWheelHolonomicDrive.java
@@ -12,6 +12,7 @@ import vision.tools.VectorGeometry;
 public class FourWheelHolonomicDrive implements DriveInterface {
     private final int FORCE_WEIGHT    = 120;
     private final int ROTATION_WEIGHT = 120;
+    private final double RECTANGULAR_DRIVE_FACTOR = 0.8;
 
     public int MAX_ROTATION = 0; //deprecated
     public int MAX_MOTION = 0; //deprecated
@@ -25,17 +26,16 @@ public class FourWheelHolonomicDrive implements DriveInterface {
 
         rotation /= Math.PI;
 
-        double front = dir.y * FORCE_WEIGHT - rotation * ROTATION_WEIGHT;
+        double front = dir.y * FORCE_WEIGHT - rotation * ROTATION_WEIGHT * RECTANGULAR_DRIVE_FACTOR;
         double left  = dir.x * FORCE_WEIGHT - rotation * ROTATION_WEIGHT;
-        double back  = dir.y * FORCE_WEIGHT + rotation * ROTATION_WEIGHT;
+        double back  = dir.y * FORCE_WEIGHT + rotation * ROTATION_WEIGHT * RECTANGULAR_DRIVE_FACTOR;
         double right = dir.x * FORCE_WEIGHT + rotation * ROTATION_WEIGHT;
-
-
 
         ((FourWheelHolonomicRobotPort) port).fourWheelHolonomicMotion(front, back, left, right);
 
     }
 
+    //not completed
     public void aim(RobotPort port, double rotation) {
         double front = -rotation;
         double back  = rotation;
