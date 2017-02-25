45ca04b87e2d0ae339de9726b3029c387b6cce10
Cole Zimmerman DICE <s1455790@sms.ed.ac.uk>
2017-02-25 18:36:16 +0000

    Modified GetOpen; WIP

--- src/strategy/actions/GetOpen.java
+++ src/strategy/actions/GetOpen.java
@@ -5,7 +5,10 @@ import strategy.points.basicPoints.RobotPoint;
 import strategy.robots.RobotBase;
 import vision.Robot;
 import vision.RobotType;
+import vision.constants.Constants;
 import vision.tools.VectorGeometry;
+
+import java.util.Queue;
 
 /**
  * When our ally has the ball, get to a position where we can be passed to.
@@ -15,9 +18,9 @@ public class GetOpen extends ActionBase {
     private final int GET_OPEN = 0;
     private final int STAY = 1;
 
-    private VectorGeometry usToFriend;
-    private Robot us;
-    private Robot friend;
+    private static VectorGeometry usToFriend;
+    private static Robot us;
+    private static Robot friend;
 
     public GetOpen(RobotBase robot) {
         super(robot);
@@ -41,15 +44,15 @@ public class GetOpen extends ActionBase {
 
     @Override
     public void tok() {
-        this.us = Strategy.world.getRobot(RobotType.FRIEND_2);
-        this.friend = Strategy.world.getRobot(RobotType.FRIEND_1);
+        us = Strategy.world.getRobot(RobotType.FRIEND_2);
+        friend = Strategy.world.getRobot(RobotType.FRIEND_1);
 
-        if (this.us == null || this.friend == null) {
+        if (us == null || friend == null) {
             return;
         }
 
-        this.usToFriend = VectorGeometry.fromTo(this.us.location, this.friend.location);
-        if (open()) {
+        usToFriend = VectorGeometry.fromTo(us.location, friend.location);
+        if (open(us.location)) {
             this.enterState(STAY);
         } else {
             this.enterState(GET_OPEN);
@@ -58,24 +61,53 @@ public class GetOpen extends ActionBase {
     }
 
     /**
-     * Returns true if Frodo is open for a pass, false otherwise.
+     * Returns true if Frodo is open/would be open for a pass if he were in this location.
      * @return
      */
-    private boolean open() {
+    private static boolean open(VectorGeometry location) {
 
         Robot foeOne = Strategy.world.getRobot(RobotType.FOE_1);
         Robot foeTwo = Strategy.world.getRobot(RobotType.FOE_2);
 
-        if (foeOne != null && VectorGeometry.closestPointToLine(us.location, usToFriend, foeOne.location).length() < 40) {
+        if (foeOne != null && VectorGeometry.closestPointToLine(location, usToFriend, foeOne.location).length() < 40) {
             return false;
         }
 
-        if (foeTwo != null && VectorGeometry.closestPointToLine(us.location, usToFriend, foeTwo.location).length() < 40) {
+        if (foeTwo != null && VectorGeometry.closestPointToLine(location, usToFriend, foeTwo.location).length() < 40) {
             return false;
         }
 
         return true;
 
+    }
+
+    /**
+     * Returns the closest point Frodo can move to to get open. Tries the closest 100 points to Frodo.
+     * WIP
+     * @return
+     */
+    private static VectorGeometry getClosestOpenPoint() {
+
+        return new VectorGeometry();
+
+    }
+
+    /**
+     * Checks whether a point is both within the pitch boundaries and if Frodo would be open if he were there.
+     * @param point
+     * @return
+     */
+    private static boolean evaluatePoint(VectorGeometry point) {
+
+        if (!open(point)) {
+            return false;
+        }
+
+        if (Math.abs(point.x) > (Constants.PITCH_WIDTH - 10) || Math.abs(point.y) > (Constants.INPUT_HEIGHT - 10)) {
+            return false;
+        }
+
+        return true;
     }
 
 }
