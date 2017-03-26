cbd604bfd59fc0c1523fb3248e2a2fe4bb4ac214
nlfox <nlfox@msn.cn>
2017-03-26 12:15:53 +0100

    start.sh modify

--- start.sh
+++ start.sh
@@ -3,6 +3,6 @@ if [  -d ./out/ ]; then
 rm -rf out/
 fi
 mkdir out
-javac  -cp "./src/:$(pwd)/libs/jssc.jar:$(pwd)/libs/v4l4j.jar:/lib/jvm/java-1.8.0-sun-1.8.0.91/jre/lib/rt.jar:$(pwd)/libs/opencv-2410.jar" src/strategy/Strategy.java -d out
+javac  -cp "./src/:$(pwd)/libs/*:$(pwd)/libs/jssc.jar:$(pwd)/libs/v4l4j.jar:/lib/jvm/java-1.8.0-sun-1.8.0.91/jre/lib/rt.jar:$(pwd)/libs/opencv-2410.jar:$(pwd)/libs/" src/strategy/Strategy.java -d out
 export LD_LIBRARY_PATH=$(pwd)/libs/
-java  -cp "./out/:./src/:$(pwd)/libs/jssc.jar:$(pwd)/libs/v4l4j.jar:/lib/jvm/java-1.8.0-sun-1.8.0.91/jre/lib/rt.jar:$(pwd)/libs/opencv-2410.jar" strategy.Strategy
+java  -cp "./out/:./src/:$(pwd)/libs/*:$(pwd)/libs/jssc.jar:$(pwd)/libs/v4l4j.jar:/lib/jvm/java-1.8.0-sun-1.8.0.91/jre/lib/rt.jar:$(pwd)/libs/opencv-2410.jar:$(pwd)/libs/" strategy.Strategy
